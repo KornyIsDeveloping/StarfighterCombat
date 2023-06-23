def5e8583f216c87ac461c577122f25415a2cae4
KornyIsDeveloping <motanutzu2@gmail.com>
2023-06-23 21:56:11 +0300

    store menu update
    added some content into the store menu

--- StarfighterCombat/Assets/Scripts/MenuButtonsHandler.cs
+++ StarfighterCombat/Assets/Scripts/MenuButtonsHandler.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class MenuButtonsHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //show elements for StoreButton
+
+    //hide elements from Menu and Settings
+    //public GameObject[] elementsToHide;
+    public GameObject[] StartGameButton;
+    public GameObject[] RightArrowButton;
+    public GameObject[] LeftArrowButton;
+    public GameObject[] CraftMenuSelector;
+    //--------------------------------------------------------------------
+    //show elements for Home
+
+    //hide elements from Store and Settigns
+    //--------------------------------------------------------------------
+    //show elements from Settings
+
+    //hide elements from Menu and Store
+    //--------------------------------------------------------------------
+    public void OnButtonClick()
     {
-        
+/*        foreach (var elementToShow in elementsToShow)
+        {
+            elementToShow.SetActive(true);
+        }*/
+         
+        foreach (var StartGameButton in StartGameButton) //menu
+        {
+            StartGameButton.SetActive(false);
+        }
+
+        foreach (var RightArrowButton in RightArrowButton) //menu
+        {
+            RightArrowButton.SetActive(false);
+        }
+        foreach (var LeftArrowButton in LeftArrowButton) //menu
+        {
+            LeftArrowButton.SetActive(false);
+        }
+        foreach (var CraftMenuSelector in CraftMenuSelector) //menu
+        {
+            CraftMenuSelector.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
