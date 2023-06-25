5af78c6eda5102086ffcdde23e5f7eaaee7570c3
KornyIsDeveloping <motanutzu2@gmail.com>
2023-06-25 16:17:45 +0300

    menu functionality done
    swapping store, home and settings menu

--- StarfighterCombat/Assets/Scripts/CanvasController.cs
+++ StarfighterCombat/Assets/Scripts/CanvasController.cs
@@ -6,16 +6,26 @@ public class CanvasController : MonoBehaviour
 {
     public Canvas MainMenuCanvas;
     public Canvas StoreMenuCanvas;
+    public Canvas SettingsMenuCanvas;
 
     public void ShowMainMenu()
     {
         MainMenuCanvas.gameObject.SetActive(true);
         StoreMenuCanvas.gameObject.SetActive(false);
+        SettingsMenuCanvas.gameObject.SetActive(false);
     }
 
     public void ShowStoreMenu()
     {
         MainMenuCanvas.gameObject.SetActive(false);
         StoreMenuCanvas.gameObject.SetActive(true);
+        SettingsMenuCanvas.gameObject.SetActive(false);
+    }
+
+    public void ShowSettingsMenu()
+    {
+        MainMenuCanvas.gameObject.SetActive(false);
+        StoreMenuCanvas.gameObject.SetActive(false);
+        SettingsMenuCanvas.gameObject.SetActive(true);
     }
 }
