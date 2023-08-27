ad49a2cd82d94cf5c823d43a0d07d6a07c1d596a
KornyIsDeveloping <motanutzu2@gmail.com>
2023-08-27 12:22:27 +0300

    clear code
    removed scripts and components

--- StarfighterCombat/Assets/Scripts/StartGame.cs
+++ StarfighterCombat/Assets/Scripts/StartGame.cs
@@ -5,18 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public CraftSelectionManager craftSelectionManager;
-
     public void PlayGame()
     {
-       /* //get the selected craft's information from PlayerPrefs
-        string selectedCraftName = PlayerPrefs.GetString("SelectedCraftName");
-        int selectedCraftLevel = PlayerPrefs.GetInt("SelectedCraftLevel");
-
-        //pass the selected craft's information to the game scene
-        PlayerPrefs.SetString("SelectedCraftNameForGame", selectedCraftName);
-        PlayerPrefs.SetInt("SelectedCraftLevelForGame", selectedCraftLevel);*/
-
         //load the next scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
